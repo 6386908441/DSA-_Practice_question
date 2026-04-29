fd196ba239e4984e99a427de9ab6f86ce0a3638d
6386908441 <rk183265@gmail.com>
2026-04-30 00:21:26 +0530

    latest code

---
+++ problem_6.py
@@ -0,0 +1,4 @@
+a = 2
+a = 4
+a = 6
+print(a + a + a) # Python reads and executes scripts from top to bottom.
---
+++ problem_9.py
@@ -0,0 +1,14 @@
+# Complete the script so that it prints out the second item of the list.
+
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+# print(letters[1])
+# print(letters[3:6])
+# print(letters[0:3])
+# print(letters[-2])
+# print(letters[-3:])
+print(letters[::2])
+# for letter in letters:
+#     if letter == "b":
+#         print(letter)
+#         break
+
